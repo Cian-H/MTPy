c1c5662db1381304e8554d212a44c6d3f0fff05f
Cian Hughes <chughes000@gmail.com>
2024-04-22 15:40:18 +0100

    Removed dask log intercepting
    Dask log intercepting was causing issues with pytest so i have removed
    it.

--- src/python/mtpy/__init__.py
+++ src/python/mtpy/__init__.py
@@ -17,13 +17,6 @@ from . import base, loaders, meltpool_tomography, proc, utils, vis
 __all__ = ["base", "loaders", "proc", "utils", "vis", "meltpool_tomography"]
 
 from .meltpool_tomography import MeltpoolTomography  # noqa
-from mtpy.utils.log_intercept import InterceptHandler, redirect_logging_to_loguru
-import dask.distributed as ddist
+from loguru import logger
 
-# Here, we redirect default logging to loguru to make logging faster
-# And easier to handle
-redirect_logging_to_loguru()
-ddist.core.logger.addHandler(InterceptHandler())
-ddist.worker.logger.addHandler(InterceptHandler())
-ddist.scheduler.logger.addHandler(InterceptHandler())
-ddist.nanny.logger.addHandler(InterceptHandler())
+logger.disable("mtpy")
