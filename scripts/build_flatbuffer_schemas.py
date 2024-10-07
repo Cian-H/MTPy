53c7f7a582882beca836f6c8d081959cfce47190
Cian Hughes <chughes000@gmail.com>
2024-10-07 12:09:22 +0100

    Added flatbuffer functionality for tree_metadata

---
+++ scripts/build_flatbuffer_schemas.py
@@ -0,0 +1,15 @@
+"""Automatically generates submodule for parsing tree metadata flatbuffer."""
+
+from pathlib import Path
+import subprocess
+
+
+root = Path(__file__).parent.parent
+
+
+def main() -> None:
+    """Automatically generates submodule for parsing tree metadata flatbuffer."""
+    subprocess.run(
+        ["flatc", "-p", "-o", "mtpy/utils/", "mtpy/utils/tree_metdata.fbs"],
+        check=True,
+    )
