5b174e803e85c292beb23c45511ff538210ef892
Cian Hughes <chughes000@gmail.com>
2024-10-09 15:46:54 +0100

    Regenerated docs.

--- scripts/gen_uml_diagrams.py
+++ scripts/gen_uml_diagrams.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import subprocess
 
 root = Path(__file__).parent.parent
-uml_dir = root / "docs/tmp/uml/"
+uml_dir = root / "docs/uml/"
 
 uml_dir.mkdir(parents=True, exist_ok=True)
 
