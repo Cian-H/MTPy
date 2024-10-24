25ce1db201addf3c14b989c668466ca084f96e46
Cian Hughes <chughes000@gmail.com>
2024-10-24 12:13:40 +0100

    Version bumps
    Version bump. Includes update of pakage versions, overhaul of pre-commit
    hooks, and update of mailmap.

--- flake.nix
+++ flake.nix
@@ -4,31 +4,55 @@
   };
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowUnfree = true;
-          cudaSupport = true;
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
         };
-      };
-    in
-    {
-      devShell = pkgs.mkShell
-        {
-          NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.stdenv.cc.cc
-          ];
-          NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
+      in {
+        devShell =
+          pkgs.mkShell
+          {
+            NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.arrow-cpp # For pyarrow
+              pkgs.stdenv.cc.cc # For cmake
+              pkgs.llvm_14 # For llvmlite
+              pkgs.python312 # For poetry
+              pkgs.zlib # For numpy
+            ];
+            NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
 
-          buildInputs = with pkgs; [
-            python312
-            flatbuffers
-          ];
-        };
-    }
-  );
+            buildInputs =
+              (with pkgs; [
+                arrow-cpp
+                python312
+                flatbuffers
+                llvm_14
+                zlib
+              ])
+              ++ (with pkgs-unstable; [
+                steam-run-free
+              ]);
+          };
+      }
+    );
 }
-
